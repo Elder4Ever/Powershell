910ffdcc561b710b82dd9d32d2cfa66614db6030
Elder4Ever <112210402+Elder4Ever@users.noreply.github.com>
2022-08-28 23:13:49 -0400

    Update Test-Languages.ps1
    Added Code to the file

--- Recon/Test-Languages.ps1
+++ Recon/Test-Languages.ps1
@@ -1,1 +1,2 @@
-
+$py = &{python3 -V} 2>&1; if($py -is [System.Management.Automation.ErrorRecord]){ $py.Exception.Message }else{ $py }
+$ps1 = &{$PSVersionTable.PSVersion} 2>&1; if($ps1 -is [System.Management.Automation.ErrorRecord]){ $ps1.Exception.Message }else{Write-Host 'Powershell' $PSVersionTable.PSVersion}
