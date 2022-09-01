0a3121354945bec8b344d03ce0fe954c8b822e44
Elder4Ever <112210402+Elder4Ever@users.noreply.github.com>
2022-09-01 17:23:29 -0400

    Grabs Cached Wifi SSID and Password

--- Recon/Invoke-WifiRecon.ps1
+++ Recon/Invoke-WifiRecon.ps1
@@ -1,3 +1,5 @@
+#Example: Invoke-WebRequest -Uri https://raw.githubusercontent.com/Elder4Ever/Powershell/main/Recon/Invoke-WifiRecon.ps1 -OutFile Invoke-WifiRecon.ps1; ./Invoke-WifiRecon.ps1 -Output C:\test.csv; Remove-Item Invoke-WifiRecon.ps1
+
 param(
     $Output
 )
@@ -11,12 +13,12 @@ if ($wifi -match "There is no wireless interface on the system."){
  
 $ListOfSSID = ($wifi | Select-string -pattern "\w*All User Profile.*: (.*)" -allmatches).Matches | ForEach-Object {$_.Groups[1].Value}
 $NumberOfWifi = $ListOfSSID.count
-Write-Warning "[$(Get-Date)] I've found $NumberOfWifi Wi-Fi Connection settings stored in your system $($env:computername) : "
+Write-Warning "[$(Get-Date)] I've found $NumberOfWifi Wi-Fi Profile(s) stored in the system"
 ForEach($SSID in $ListOfSSID){
 
         $passphrase = ($(netsh.exe wlan show profiles name=`"$SSID`" key=clear) | Select-String -pattern ".*Key Content.*:(.*)" -allmatches).Matches | ForEach-Object {$_.Groups[1].Value}
         $Auth = ($(netsh.exe wlan show profiles name=`"$SSID`" key=clear) | Select-String -pattern ".*Authentication.*:(.*)" -AllMatches).Matches | ForEach-Object {$_.Groups[1].Value}
         $Auth = $Auth -split "  "
         $List = [PSCustomObject]@{"PROFILE_NAME"=$SSID;"Authentication"=$Auth[0];"PASSWORD"=$passphrase}
-        $List | Export-Csv -Path "$Output.csv" -Append -NoTypeInformation
+        $List | Export-Csv -Path "$Output" -Append -NoTypeInformation
 }
